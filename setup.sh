6bd51e6bde08f46f04c7de6f0c1d9997e65895f2
Asa Gilmore <asaagilmore@gmail.com>
2024-02-01 11:09:07 -0800

    updated setup

--- setup.sh
+++ setup.sh
@@ -1,20 +1,6 @@
-#!/bin/bash
-sudo apt update
-sudo apt install git curl
-
-# Install miniConda
-mkdir -p ~/miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
-bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-rm -rf ~/miniconda3/miniconda.sh
-
-~/miniconda3/bin/conda init bash
-
-# Source the bashrc file to apply the changes immediately
-source ~/.bashrc
-
 # Create a new environment
 conda create -n dipy python=3.11
+conda init bash
 conda activate dipy
 
 # Install the required packages
