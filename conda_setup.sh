520f5fe18abd8b89fd9316c4ee082afddaf95700
Asa Gilmore <asaagilmore@gmail.com>
2024-02-01 11:38:48 -0800

    more conda fix

--- conda_setup.sh
+++ conda_setup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 cd ~
 sudo apt update
-sudo apt install git curl
+sudo apt install git wget
+
+# Get the absolute path of the home directory
+HOME_DIR=$(eval echo ~$USER)
 
 # Install miniConda
-mkdir -p ~/miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
-bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-rm -rf ~/miniconda3/miniconda.sh
+mkdir -p $HOME_DIR/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $HOME_DIR/miniconda3/miniconda.sh
+bash $HOME_DIR/miniconda3/miniconda.sh -b -u -p $HOME_DIR/miniconda3
+rm -rf $HOME_DIR/miniconda3/miniconda.sh
 
-~/miniconda3/bin/conda init bash
+$HOME_DIR/miniconda3/bin/conda init bash
